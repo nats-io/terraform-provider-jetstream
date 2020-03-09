f2ac394f9b13747b95a4472f9bb3bbbea1b42c00
R.I.Pienaar <rip@devco.net>
2020-03-09 14:55:33 +0100

    initial support for creds as variables

--- jetstream/jetstream.go
+++ jetstream/jetstream.go
@@ -3,6 +3,8 @@ package jetstream
 import (
 	"regexp"
 
+	"github.com/nats-io/jwt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/nats-io/jsm.go"
@@ -28,6 +30,11 @@ func Provider() terraform.ResourceProvider {
 				Description: "Path to the NATS 2.0 credentials file to use for authentication",
 				DefaultFunc: schema.EnvDefaultFunc("NATS_CREDS", nil),
 			},
+			"credential_data": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The contents of the NATS 2.0 Credentials file to use",
+			},
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
@@ -42,12 +49,34 @@ func Provider() terraform.ResourceProvider {
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	creds := d.Get("credentials").(string)
+	credData := []byte(d.Get("credential_data").(string))
 	servers := d.Get("servers").(string)
 
 	var opts []nats.Option
 
-	if creds != "" {
+	switch {
+	case creds != "":
 		opts = append(opts, nats.UserCredentials(creds))
+
+	case len(credData) > 0:
+		defer wipeSlice(credData)
+
+		userCB := func() (string, error) {
+			return jwt.ParseDecoratedJWT(credData)
+		}
+
+		sigCB := func(nonce []byte) ([]byte, error) {
+			kp, err := jwt.ParseDecoratedNKey(credData)
+			if err != nil {
+				return nil, err
+			}
+			defer kp.Wipe()
+
+			return kp.Sign(nonce)
+		}
+
+		opts = append(opts, nats.UserJWT(userCB, sigCB))
+
 	}
 
 	err := jsm.Connect(servers, opts...)
